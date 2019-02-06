f66a2e23542bb27435d1e2edac95dd0af153a923
infactum <infactum@gmail.com>
2019-02-06 23:22:38 +0500

    Squashed 'libtgvoip/' changes from 6053cf5..cfd62e6
    cfd62e6 Why did it change the OS X project
    3a58a16 2.4.3
    c4a48b3 Updated OS X project
    564eada Fix #63
    4f64e2e fixes
    0c732e2 fixes
    12e76ed better logging
    f015b79 Merge pull request #62 from xvitaly/big-endian
    a1df90f Set preferred audio session parameters on iOS
    59a975b Fixes
    8fd89fc Fixes, mic level testing and volume adjustment
    243acfa Backported WebRTC upstream patch with Big Endian support.
    fed3bb7 Detect when proxy does not support UDP and persist that across calls
    a7546d4 Merge commit '6d03dd9ae4bf48d7344341cdd2d055ebd3a6a42e' into public
    6d03dd9 version
    69adf70 Use server config for APM + iOS crash fix
    0b42ec8 Update iOS project
    f1b9e63 packet logging
    beeea45 I apparently still suck at C++ memory management
    24fceba Update project
    7f54b91 crash fix
    f85ce99 Save more data in data saving mode
    f4c4f79 Collect packet stats and accept json string for server config
    78e584c New protocol version: optimized packet size
    8cf9177 Fixed build on iOS
    9dd089d fixed build on android
    5caaaaf Updated WebRTC APM
    cc0cf35 fixed deadlock
    02f4835 Rearranged VoIPController methods and added sections
    912f73d Updated OS X project
    39376df Fixed audio glitches on Windows
    dfe1f03 Updated project
    81daf3f fix
    296187a Merge pull request #58 from telegramdesktop/tdesktop
    44956ac Merge pull request #57 from UnigramDev/public
    fb0a2b0 Fix build for Linux.
    d6cf1b7 Updated UWP wrapper
    0f06289 Merge branch 'public' of github.com:grishka/libtgvoip into public
    dcfad91 Fix #54
    162f447 Merge pull request #56 from telegramdesktop/tdesktop
    a7ee511 Merge remote-tracking branch 'origin/tdesktop' into HEAD
    467b148 Removed unused files
    b1a0b3d 2.3
    9b292fd Fix warning in Xcode 10.
    8d8522a Merge pull request #53 from UnigramDev/public
    646f7d6 Merge branch 'public' into public
    14d782b Fixes
    68acf59 Added GetSignalBarsCount and GetConnectionState to CXWrapper
    761c586 Added GetStats to CXWrapper
    f643b02 Prevent crash if UWP WASAPI devices aren't found
    b2ac10e Fixed UWP project
    9a1ec51 Fixed build for Windows Phone, fixed some warnings
    4aea54f fix
    
    git-subtree-dir: libtgvoip
    git-subtree-split: cfd62e66a825348ac51f49e5d20bf8827fef7a38

--- PrivateDefines.h
+++ PrivateDefines.h
@@ -26,13 +26,16 @@
 #define IS_MOBILE_NETWORK(x) (x==NET_TYPE_GPRS || x==NET_TYPE_EDGE || x==NET_TYPE_3G || x==NET_TYPE_HSPA || x==NET_TYPE_LTE || x==NET_TYPE_OTHER_MOBILE)
 
 #define PROTOCOL_NAME 0x50567247 // "GrVP" in little endian (reversed here)
-#define PROTOCOL_VERSION 6
+#define PROTOCOL_VERSION 8
 #define MIN_PROTOCOL_VERSION 3
 
 #define STREAM_DATA_FLAG_LEN16 0x40
 #define STREAM_DATA_FLAG_HAS_MORE_FLAGS 0x80
-#define STREAM_DATA_XFLAG_KEYFRAME 0x01
-#define STREAM_DATA_XFLAG_FRAGMENTED 0x02
+// Since the data can't be larger than the MTU anyway,
+// 5 top bits of data length are allocated for these flags
+#define STREAM_DATA_XFLAG_KEYFRAME (1 << 15)
+#define STREAM_DATA_XFLAG_FRAGMENTED (1 << 14)
+#define STREAM_DATA_XFLAG_EXTRA_FEC (1 << 13)
 
 #define STREAM_TYPE_AUDIO 1
 #define STREAM_TYPE_VIDEO 2
@@ -41,9 +44,12 @@
 
 #define CODEC_OPUS_OLD 1
 #define CODEC_OPUS FOURCC('O','P','U','S')
+
 #define CODEC_AVC FOURCC('A','V','C',' ')
 #define CODEC_HEVC FOURCC('H','E','V','C')
 #define CODEC_VP8 FOURCC('V','P','8','0')
+#define CODEC_VP9 FOURCC('V','P','9','0')
+#define CODEC_AV1 FOURCC('A','V','0','1')
 
 #define DEFAULT_MTU 1100
 
@@ -75,6 +81,17 @@
 
 #define INIT_FLAG_DATA_SAVING_ENABLED 1
 #define INIT_FLAG_GROUP_CALLS_SUPPORTED 2
+#define INIT_FLAG_VIDEO_SEND_SUPPORTED 4
+#define INIT_FLAG_VIDEO_RECV_SUPPORTED 8
+
+#define INIT_VIDEO_RES_NONE 0
+#define INIT_VIDEO_RES_240 1
+#define INIT_VIDEO_RES_360 2
+#define INIT_VIDEO_RES_480 3
+#define INIT_VIDEO_RES_720 4
+#define INIT_VIDEO_RES_1080 5
+#define INIT_VIDEO_RES_1440 6
+#define INIT_VIDEO_RES_4K 7
 
 #define TLID_DECRYPTED_AUDIO_BLOCK 0xDBF948C1
 #define TLID_SIMPLE_AUDIO_BLOCK 0xCC0D0E76
@@ -86,7 +103,7 @@
 #define TLID_VECTOR 0x1cb5c415
 #define PAD4(x) (4-(x+(x<=253 ? 1 : 0))%4)
 
-#define MAX_RECENT_PACKETS 64
+#define MAX_RECENT_PACKETS 128
 
 #define MAX(a,b) (a>b ? a : b)
 #define MIN(a,b) (a<b ? a : b)
@@ -106,5 +123,11 @@ inline bool seqgt(uint32_t s1, uint32_t s2){
 	return ((s1>s2) && (s1-s2<=SEQ_MAX/2)) || ((s1<s2) && (s2-s1>SEQ_MAX/2));
 }
 
+#define NEED_RATE_FLAG_SHITTY_INTERNET_MODE 1
+#define NEED_RATE_FLAG_UDP_NA 2
+#define NEED_RATE_FLAG_UDP_BAD 4
+#define NEED_RATE_FLAG_RECONNECTING 8
+
+#define VIDEO_FRAME_FLAG_KEYFRAME 1
 
 #endif //TGVOIP_PRIVATEDEFINES_H
