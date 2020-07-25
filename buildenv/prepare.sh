dc35362f1cde289cee656d869e30d23bd82f0d85
infactum <infactum@gmail.com>
2020-07-25 19:03:38 +0300

    update workflow

--- buildenv/prepare.sh
+++ buildenv/prepare.sh
@@ -4,7 +4,7 @@ set -e
 
 DOCKER_IMAGE=infactum/tg2sip-builder
 
-for DOCKER_TAG in bionic centos6 centos7
+for DOCKER_TAG in bionic centos7
 do
     docker build . -f Dockerfile."$DOCKER_TAG" -t "$DOCKER_IMAGE:$DOCKER_TAG"
     docker push "$DOCKER_IMAGE:$DOCKER_TAG"
